3fa6464e9290299ac213e5b03529542aa2ad9509
Pascal <pascalkknde@Pascal.localdomain>
2022-11-08 10:58:04 +0000

    create middleware

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser')
+const {logger} = require("./middleware/logger");
+const errorHandler = require('./middleware/errorHamdler')
+const PORT = process.env.PORT || 3500
+
+app.use(logger);
+app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static('./routes/root.js'))
+app.all('*', (req, res)=>{
+    res.status(404);
+    if (req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }else if (req.accepts('json')){
+        res.json({message:'404 Not Found'})
+    }else{
+        res.type('txt').send('404 Not Found')
+    }
+})
+app.use(errorHandler)
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+
+module.exports = app;
